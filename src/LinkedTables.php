51c033ea87351c86c978bc7544687547d699dbd9
Domenico Giambra <zorinik@gmail.com>
2023-01-09 18:11:31 +0100

    Cache linked tables

--- src/LinkedTables.php
+++ src/LinkedTables.php
@@ -1,5 +1,6 @@
 <?php namespace Model\LinkedTables;
 
+use Model\Cache\Cache;
 use Model\Db\DbConnection;
 use Model\ProvidersFinder\Providers;
 
@@ -10,15 +11,26 @@ class LinkedTables
 	public static function getTables(DbConnection $db): array
 	{
 		if (!isset(self::$cache[$db->getName()])) {
-			self::$cache[$db->getName()] = [];
-			$providers = Providers::find('LinkedTablesProvider');
-			foreach ($providers as $provider) {
-				$providerTables = $provider['provider']::tables()[$db->getName()] ?? [];
-				foreach ($providerTables as $k => $v)
-					self::$cache[$db->getName()][is_numeric($k) ? $v : $k] = is_numeric($k) ? $v . '_custom' : $v;
-			}
+			$cache = Cache::getCacheAdapter();
+			self::$cache[$db->getName()] = $cache->get('model.linked-tables.tables.' . $db->getName(), function (\Symfony\Contracts\Cache\ItemInterface $item) use ($db) {
+				$item->expiresAfter(3600 * 24);
+				return self::doGetTables($db);
+			});
 		}
 
 		return self::$cache[$db->getName()];
+	}
+
+	private static function doGetTables(DbConnection $db): array
+	{
+		$tables = [];
+		$providers = Providers::find('LinkedTablesProvider');
+		foreach ($providers as $provider) {
+			$providerTables = $provider['provider']::tables()[$db->getName()] ?? [];
+			foreach ($providerTables as $k => $v)
+				$tables[is_numeric($k) ? $v : $k] = is_numeric($k) ? $v . '_custom' : $v;
+		}
+
+		return $tables;
 	}
 }
